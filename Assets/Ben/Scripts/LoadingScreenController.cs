2f9855263a2c18c2312752c7f168c7bb3844ea40
Benadict <115377654+2239356Benadict@users.noreply.github.com>
2023-01-24 20:36:01 +0000

    3D Environment modified
    After more 3D models were added[D].

---
+++ Assets/Ben/Scripts/LoadingScreenController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScreenController : MonoBehaviour
+{
+    public Renderer screenRenderer;
+
+    public float fadeDuration;
+    public Color fadeColor;
+
+    public bool fadeOnStart;
+    private void Start()
+    {
+        screenRenderer = GetComponent<Renderer>();
+        if (fadeOnStart)
+            FadeIn();
+    }
+
+    public void FadeIn()
+    {
+        Fade(1, 0);
+    }
+    public void FadeOut()
+    {
+        Fade(0, 1);
+    }
+
+    public void Fade(float alphaIn, float alphaOut)
+    {
+        StartCoroutine(FadeRoutine(alphaIn, alphaOut));
+    }
+
+    public IEnumerator FadeRoutine(float alphaIn, float alphaOut)
+    {
+        float timer = 0;
+        while(timer <= fadeDuration)
+        {
+            Color newColor1 = fadeColor;
+            newColor1.a = Mathf.Lerp(alphaIn, alphaOut, timer / fadeDuration);
+            screenRenderer.material.SetColor("_BaseColor", newColor1);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        Color newColor2 = fadeColor;
+        newColor2.a = alphaOut;
+        screenRenderer.material.SetColor("_Color", newColor2);
+
+    }
+}
