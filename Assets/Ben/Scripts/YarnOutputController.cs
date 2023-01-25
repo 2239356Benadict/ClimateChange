b7e5e370985c58efc732b894a910281e5932dbac
Benadict <115377654+2239356Benadict@users.noreply.github.com>
2023-01-25 15:45:11 +0000

    Yarn Audio
    Yarn with audio and consequence is done.

--- Assets/Ben/Scripts/YarnOutputController.cs
+++ Assets/Ben/Scripts/YarnOutputController.cs
@@ -5,7 +5,7 @@ using Yarn.Unity;
 
 public class YarnOutputController : MonoBehaviour
 {
-    public GameObject badScenario;
+    public GameObject badScenarioObjects;
     public GameObject entryEnvironmentObjects;
     public GameObject endPanel;
     public WasteCollectedStatus collectedStatus;
@@ -23,17 +23,14 @@ public class YarnOutputController : MonoBehaviour
     {
         bool answerNo;
         yarnMemory.TryGetValue("$notReady", out answerNo);
-        string playerReply;
-        yarnMemory.TryGetValue("$notReadyToProceed", out playerReply);
-        Debug.Log(playerReply);
-        Debug.Log(answerNo);
 
-        if (playerReply == "True")
+
+        if (answerNo)
         {
-            badScenario.SetActive(true);
-            entryEnvironmentObjects.SetActive(false);
-            endPanel.SetActive(true);
-            Debug.Log(playerReply);
+            collectedStatus.WasteCollectionStatus();
+            //badScenarioObjects.SetActive(true);
+            //entryEnvironmentObjects.SetActive(false);
+            //endPanel.SetActive(true);
         }
     }
 }
